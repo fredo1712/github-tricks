b4e55dae251d1d24fe0711d6329f2943f97188fc
frederic <pierrefrederic.jb@gmail.com>
2018-10-28 18:22:06 -0400

    add test python file

---
+++ helloworld.py
@@ -0,0 +1,32 @@
+##my_string = "Hello World"
+##print(my_string)
+##
+##var1 = "Allo \"Haiti est un pays de merde\" a dit Trump"
+##print(var1)
+##
+##
+##name = 'Phanou'
+##greeting = f'Hello, {name}'
+##
+##print(greeting)
+
+##your_name = input("Please enter your name: ")
+##print (f'Hello, {your_name}')
+##
+##age = int(input("Please enter your age: "))
+##print (f'you have lived for {age * 12} month')
+
+
+##name = input ("Please enter your name: ")
+##print("Hello, " + name)
+##
+##age = int(input("Please enter your age: "))
+##print (age * 12)
+
+age = 34
+is_over_age = age >= 30
+is_under_age = age < 30
+is_age = age == 34
+print (is_over_age)
+print (is_under_age)
+print (is_age)
